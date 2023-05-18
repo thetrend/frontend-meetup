a8205af5d4c694d7535bab8742f74bdaae38089c
Grace de la Mora <thetrend@users.noreply.github.com>
2023-05-18 19:36:53 -0400

    error, naming

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
 import App from './App.tsx'
+import Error from './Error'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/:displayName?',
+    element: <App />,
+    errorElement: <Error />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
